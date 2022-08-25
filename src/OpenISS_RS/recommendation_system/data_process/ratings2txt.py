c0743b84f95ed313c59aa8f388ed5821b60c12d6
carlos <carlos@encs.concordia.ca>
2022-08-25 10:49:06 -0400

    files location + Graph params. fixed

--- src/OpenISS_RS/recommendation_system/data_process/ratings2txt.py
+++ src/OpenISS_RS/recommendation_system/data_process/ratings2txt.py
@@ -1,7 +1,8 @@
 from py2neo import Graph, Node, Relationship,NodeMatcher,cypher
 
 def save_rating_triples(username,password):
-    graph = Graph('http://localhost:7474', username= username, password=password)
+    #graph = Graph('http://localhost:7474', username= username, password=password)
+    graph = Graph(host="localhost", auth=(username, password))
     string_qurey = "match (a)-[r]->(b) where type(r) in ['1','2','3','4','5'] return a.name,type(r),b.name;"
     query_result = graph.run(string_qurey)
     # print(query_result.data())
@@ -15,8 +16,8 @@ def save_rating_triples(username,password):
         f.write(result)
         f.close()
 
-username = "neo4j"
-password = "0905"
-save_rating_triples("neo4j","0905")
+username = "userx"
+password = "usuariox"
+save_rating_triples(username,password)
 
 
